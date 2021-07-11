1e1ba362cef4d95aca829f1f8b52b5a45deb9fdf
maltekliemann <mail@maltekliemann.com>
2021-07-11 22:59:53 +0200

    Update commandline tests

---
+++ resources/example.h
@@ -0,0 +1,47 @@
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace outer { namespace inner {
+
+class Base {
+};
+
+template<typename T, typename... Ts>
+class Derived : public Base {
+  Q_OBJECT
+
+public:
+  Derived() = delete;
+  template<typename... Us>
+  Derived(Us&&... us) : Base{} {
+    ((void)us, ...);
+  }
+  Derived(int) : Base{} {
+  }
+
+  std::shared_ptr<std::string> method_decl() const volatile;
+
+  int virtual virtual_method(float, std::string) {
+    return 1;
+  }
+
+protected:
+  float virtual pure_virtual_method(double a1, std::shared_ptr<std::vector<unsigned int>> a2) const = 0;
+
+  void slot_decl(int, double, std::shared_ptr<int>) = 0;
+
+private:
+  template<typename U, typename... Us>
+  U template_method(Us&&... us) {
+    return U{std::forward<Us>(us)...};
+  }
+
+  static int static_method() {
+    return 3;
+  }
+
+  void signal_decl(int, float, std::vector<int>);
+};
+
+}} // namespace outer::inner
--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -2,6 +2,9 @@
 # 
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import tempfile
+import os
+
 import pytest
 
 from drmock import commandline
@@ -9,18 +12,25 @@ from drmock import generator
 from drmock import utils
 
 
+def test_example(script_runner):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        path = os.path.join(tmpdir, 'example_mock.h')
+        ret = script_runner.run('drmock-gen', '--input-class', 'Derived', '--output-class', 'DerivedMock', 'resources/example.h', str(path), '--std=c++17')
+    assert ret.success
+
+
 def test_success(monkeypatch, mocker, script_runner):
     args, compiler_flags = object(), object()
     monkeypatch.setattr(commandline, 'parse_args', mocker.Mock(return_value=(args, compiler_flags)))
     monkeypatch.setattr(generator, 'main', mocker.Mock())
-    ret = script_runner.run('drmock-gen', print_result=False)
+    ret = script_runner.run('drmock-gen')
     assert ret.success
     assert generator.main.called_once_with(args, compiler_flags)
 
 
 def test_parser_fails(monkeypatch, mocker, script_runner):
     # Cause a parser error by not providing required args.
-    ret = script_runner.run('drmock-gen', print_result=False)
+    ret = script_runner.run('drmock-gen')
     assert not ret.success
     assert ret.returncode == 2
 
